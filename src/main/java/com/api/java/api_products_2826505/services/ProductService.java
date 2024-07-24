05fb6c7806c4c300adef2bdd3803405209d85625
Sara <amartinezsana@gmail.com>
2024-07-24 17:45:11 -0500

    servicio crado

---
+++ src/main/java/com/api/java/api_products_2826505/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.api.java.api_products_2826505.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.java.api_products_2826505.entities.Product;
+import com.api.java.api_products_2826505.repositories.ProductRepository;
+
+@Service
+public class ProductService  {
+ 
+    //crear metodos para la CRUD
+    //que estan en el repositorio
+    
+   @Autowired
+   private ProductRepository repository;
+
+   //metodos
+   //READ
+   List<Product> findAll(){
+    return  (List<Product>) repository.findAll();
+   }
+}
